0f600a099e417c6726da76e86dbed115adc61d5c
farruhzoirov <fzoirov29@gmail.com>
2025-08-06 22:03:20 +0500

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadModule } from './upload/upload.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: 300000, // 5-minute
-          limit: 10,
-        },
-      ],
-    }),
-    UploadModule,
-  ],
+  imports: [UploadModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
